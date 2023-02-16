b07a38338090591ad23a9d60b9b5f61409f200fc
jmehlberg96 <meh22002@byui.edu>
2023-02-15 20:10:09 -0700

    fixed javascript code

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -28,30 +28,23 @@ document.querySelector(".banner-button").addEventListener('click', function() {
     this.closest(".banner-wrapper").style.display = "none";
 });
 
-// initialize display elements
 const todayDisplay = document.querySelector(".today");
 const visitsDisplay = document.querySelector(".visits");
 
-// get the stored value in localStorage
+
 let numVisits = Number(window.localStorage.getItem("visits-ls"));
 
-// determine if this is the first visit or display the number of visits.
 if (numVisits !== 0) {
 	visitsDisplay.textContent = numVisits;
 } else {
 	visitsDisplay.textContent = `_`;
 }
-
-// increment the number of visits.
 numVisits++;
-// store the new number of visits value
 localStorage.setItem("visits-ls", numVisits);
-// show todays date.
 
-let d = new Date();
-let lastTime = d.setTime(Date.now());
-let daysSince = (lastTime - Date.now()) / 84600000;
+var lastTime = new Date(document.lastModified).getTime();
+let daysSince = (Date.now() - lastTime) / 84600000;
+let rounded = Math.round(daysSince);
+todayDisplay.textContent = rounded;
 
-todayDisplay.textContent = daysSince;
 
-// You can view the localStorage data using the Applications panel in the browsers's DevTools.
